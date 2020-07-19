8cf5a20d4289c2977dc42f3c185d34f3f9ba39c6
23kertesz23 <64039821+23kertesz23@users.noreply.github.com>
2020-07-19 17:50:26 -0400

    mi primer commit

---
+++ Asignacion/6.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "rand() Esta función se utiliza para poder generar enteros aleatoreos<br/>";
+
+			echo "<br> <br>";
+
+			echo "Generar un número aleatoreo:";
+
+			echo "Número: ". rand();
+
+			echo "<br> <br>";
+
+			echo "Generar un número aleatoreo entre el 56 y el 76:";
+
+			echo "Número: ". rand(56, 76);
+
+			echo "<br> <br>";
+
+			echo "Generar un número aleatoreo entre el -23 y el -41:";
+
+			echo "Número: ". rand(-23, -41);
+?>
